66a727981982f2b53a07ebdfc147110f47e2018d
Yura Movsesyan <user_yura@outlook.com>
2022-08-05 23:11:05 +0300

    added abstract class Element

---
+++ MultiTool/TUI/Element.cs
@@ -0,0 +1,42 @@
+﻿namespace TUI;
+
+public abstract class Element
+{
+    private int PositionLeft, PositionTop;
+    private int oldPositionLeft, oldPositionTop;
+
+    public Element(bool isNewLine = true, bool isEndl = true)
+    {
+
+        if (isNewLine) Console.Write('\n');
+
+        PositionLeft = 0;
+        PositionTop = Console.CursorTop;
+
+        Draw();
+
+        if (isEndl) Console.Write('\n');
+    }
+
+    public void DrawRun()
+    {
+        DrawStart();
+        Draw();
+        DrawEnd();
+    }
+
+    private void DrawStart()
+    {
+        oldPositionLeft = Console.CursorLeft;
+        oldPositionTop = Console.CursorTop;
+
+        Console.SetCursorPosition(PositionLeft, PositionTop);
+    }
+
+    private void DrawEnd()
+    {
+        Console.SetCursorPosition(oldPositionLeft, oldPositionTop);
+    }
+
+    protected abstract void Draw();
+}
